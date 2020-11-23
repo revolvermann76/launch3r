08d8bc3f195f7ab2ee3bb3c6a8a6ac66ab667198
revolvermann76 <postkutsche@gmail.com>
2020-11-23 14:07:09 +0100

    ...

--- Neuer Ordner/launch3r.js
+++ Neuer Ordner/launch3r.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const ioHook = require('ioHook');
-
+/*
 ioHook.on("mousedown",function(msg){console.log(msg);});
 
 ioHook.on("keypress",function(msg){console.log(msg);});
 
 ioHook.on("keydown",function(msg){console.log(msg);});
-
-ioHook.on("keyup",function(msg){console.log(msg);});
-
+*/
+ioHook.on("keyup",function(msg){
+  if(msg.keycode === 68){
+    console.log("F10");
+  }
+});
+/*
 ioHook.on("mouseclick",function(msg){console.log(msg)});
 
 ioHook.on("mousewheel",function(msg){console.log(msg)});
@@ -17,7 +21,7 @@ ioHook.on("mousewheel",function(msg){console.log(msg)});
 ioHook.on("mousemove",function(msg){console.log(msg)});
 
 ioHook.on("mousedrag",function(msg){console.log(msg)});
-
+*/
 //start ioHook
 ioHook.start();
 // ioHook.setDebug(true); // Uncomment this line for see all debug information from iohook
@@ -25,7 +29,7 @@ ioHook.start();
 const CTRL = 29;
 const ALT = 56;
 const F7 = 65;
-
+/*
 ioHook.registerShortcut([CTRL, F7], (keys) => {
   console.log('Shortcut pressed with keys:', keys);
 });
@@ -36,3 +40,4 @@ let shId = ioHook.registerShortcut([ALT, F7], (keys) => {
 })
 
 console.log('Hook started. Try type something or move mouse');
+*/
